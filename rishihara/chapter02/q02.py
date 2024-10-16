7e3da9e23b918e7f637a496893da717c957c63ed
ren4325 <isihararen0@gmail.com>
2024-10-16 15:26:25 +0900

    02章q02を追加

--- rishihara/chapter02/q02.py
+++ rishihara/chapter02/q02.py
@@ -1,31 +1,27 @@
 #5.0<float>
 1.2 + 3.8
-type(1.2 + 3.8)
-
+print(type(1.2 + 3.8))
 #0<int>
 10 // 100
-type(10 // 100)
-
+print(type(10 // 100))
 #True<bool>
 1 >= 0
-type(1 >= 0)
-
+print(type(1 >= 0))
 #True<bool>
 "Hello World" == "Hello World"
-type("Hello World" == "Hello World")
-
+print(type("Hello World" == "Hello World"))
 #False<bool>
 not "Chainer" != "Tutorial"
-type(not "Chainer" != "Tutorial")
-
+print(type(not "Chainer" != "Tutorial"))
 #False<bool>
 all([True, True, False])
-type(all([True, True, False]))
-
+print(type(all([True, True, False])))
 #True<bool>
 any([True, True, False])
-type(any([True, True, False]))
-
+print(type(any([True, True, False])))
+#3<int>
+abs(-3)
+print(type(abs(-3)))
 #error
 #2 // 0
-#type(2 // 0)
+#type(2 /// 0)
