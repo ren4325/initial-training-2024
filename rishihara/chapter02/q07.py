b4e366313c3df900a9c26d9cf52e18630c6fbc61
ren4325 <isihararen0@gmail.com>
2024-10-30 13:37:15 +0900

    02章にq07を追加

---
+++ rishihara/chapter02/q07.py
@@ -0,0 +1,21 @@
+#予想:[6, 7, 8][1, 2, 3, 1]と表示される
+def f(a):
+    a = [6, 7, 8]
+    print(a)
+
+def g(a):
+    a.append(1)
+
+def somefunction():
+    a0 = [1, 2, 3]
+    f(a0)
+    print(a0)
+
+    a1 = [1, 2, 3]
+    g(a1)
+    print(a1)
+
+somefunction()
+
+#考察
+#関数内で代入をすると
